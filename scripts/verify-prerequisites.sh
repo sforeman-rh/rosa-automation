ae59feaaf39e5c56cec3fa7e8c7ca41ba854b7cb
Marc Nozell <marc@nozell.com>
2024-04-02 15:59:24 -0400

    fix cli checks

--- scripts/verify-prerequisites.sh
+++ scripts/verify-prerequisites.sh
@@ -8,7 +8,16 @@ exists()
 for i in rosa git oc terraform ocm
 do
 	if ! exists $i; then
-		exit 1
+		if [ "$1" = "terraform" ]; then
+			echo "WARNING: Install $1 from:"
+			echo "	https://developer.hashicorp.com/terraform/install"
+		fi
+		if [ "$1" = "git" ]; then
+			echo "WARNING: Install $1 via your platforms package manager or from:"
+			echo "	https://git-scm.com/downloads/guis/"
+		fi
+		echo "WARNING: Install $i from: "
+		echo "	https://console.redhat.com/openshift/downloads"
 	fi
 done
 exit 0
